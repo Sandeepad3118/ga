a2de98a371934b86c1ca0104d1ffe8fb5ad29177
Sandeep_Pc <sandeepad3118@gmail.com>
2020-08-22 00:28:42 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default import React, { Component } from 'react'
+import PropTypes from 'prop-types
+
+
+console.log(osjfoajof);
